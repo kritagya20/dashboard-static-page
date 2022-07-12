2778bf2146e5dac64b02f2acd76cc43dd520f6dc
Kritagya Singh Chouhan <88777920+kritagya20@users.noreply.github.com>
2022-07-12 18:02:19 +0530

    Add files via upload

---
+++ dashboard/js/orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'HP Laptop',
+        productNumber: '97630',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'VOODANIA Official Branded Football',
+        productNumber: '51038',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'pTron Bassbuds ',
+        productNumber: '74559',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'The Millionaire Fastlane',
+        productNumber: '30214',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Echo Dot',
+        productNumber: '77879',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'OnePlus 9R 5G',
+        productNumber: '64700',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Canon Pixma',
+        productNumber: '84257',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
---
+++ dashboard/js/script.js
@@ -0,0 +1,36 @@
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const themeToggler = document.querySelector('.theme-toggler');
+
+
+
+//SHOW-SIDEBAR
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+//CLOSE-SIDEBAR
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display= 'none';
+})
+
+//CHANGE-THEME
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
+//GENERATING-TABLE
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = ` <td>${order.productName}</td>
+                        <td>${order.productNumber}</td>
+                        <td>${order.paymentStatus}</td>
+                        <td class="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'pending' ? 'warning' : 'primary'} ">${order.shipping}</td>
+                        <td class="primary">Details</td> `;
+    tr.innerHTML = trContent;     
+    document.querySelector('table tbody').appendChild(tr);           
+})
